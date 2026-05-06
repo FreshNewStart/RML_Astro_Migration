afceef22cd0d2798b799d481119ee6460a88bc15
FreshNewStart <mike.aswad.j@gmail.com>
2026-05-06 22:15:04 +1000

    Astro / Gihub Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://FreshNewStart.github.io',
+  base: '/RML_Astro_Migration/',
+});
